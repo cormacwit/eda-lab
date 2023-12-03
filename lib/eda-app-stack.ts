669dbb09787692acad1283764c9644df65b04bdb
cormacwit <74902128+cormacwit@users.noreply.github.com>
2023-12-03 18:57:12 +0000

    Completed Fan out implementation - added emailing logic!

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -33,6 +33,11 @@ export class EDAAppStack extends cdk.Stack {
     const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
       receiveMessageWaitTime: cdk.Duration.seconds(10),
     });
+    
+
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
 
     const newImageTopic = new sns.Topic(this, "NewImageTopic", {
       displayName: "New Image topic",
@@ -52,6 +57,25 @@ export class EDAAppStack extends cdk.Stack {
       }
     );
 
+    const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
+
+    mailerFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ses:SendEmail",
+          "ses:SendRawEmail",
+          "ses:SendTemplatedEmail",
+        ],
+        resources: ["*"],
+      })
+    );
+
     // Event triggers
 
     imagesBucket.addEventNotification(
@@ -63,10 +87,19 @@ export class EDAAppStack extends cdk.Stack {
       new subs.SqsSubscription(imageProcessQueue)
     );
 
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
     const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
       batchSize: 5,
       maxBatchingWindow: cdk.Duration.seconds(10),
     });
+
+    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(10),
+    }); 
+
+    mailerFn.addEventSource(newImageMailEventSource);
 
     processImageFn.addEventSource(newImageEventSource);
 
